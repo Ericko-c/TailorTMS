7a9e8776598c1e757fdf8b46ef0b05bd8e5ba5de
wilfredmutwiri <wilfredmutwiri20@gmail.com>
2025-07-09 22:00:56 +0300

    Added new expo app

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/tailorTMS', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
---
+++ Frontend_App/TailorTMS/app/(tabs)/_layout.jsx
@@ -0,0 +1,140 @@
+import { Tabs, useLocalSearchParams,router} from 'expo-router';
+import Toast from 'react-native-toast-message';
+import {TouchableOpacity, View} from 'react-native'
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import useAuthStore from '../../zustand/store';
+import React, { useEffect } from "react";
+import axios from 'axios'
+import {SERVER_URI} from '../../constants/SERVER_URI.jsx';
+
+export default function TabsLayout() {
+const {id}=useLocalSearchParams();
+const { hasUnreadNotifications, setHasUnreadNotifications,station} = useAuthStore();
+
+  useEffect(() => {
+    if (!station) return;
+
+    const getUserNotifications = async () => {
+      try {
+        const response = await axios.get(`${SERVER_URI}/api/v1/user/notifications/${station.id}`);
+        if (response.status === 200) {
+          const unread = response.data.some((n) => n.read === false);
+          setHasUnreadNotifications(unread);
+        }
+      } catch (error) {
+        console.log("Error fetching notifications", error);
+      }
+    };
+
+    getUserNotifications(); // fetch once immediately
+    const interval = setInterval(getUserNotifications, 30000); // poll every 30 sec
+    return () => clearInterval(interval);
+  }, [station]);
+
+return (
+<View style={{flex:1}}>
+    <Tabs
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#145A32',
+          shadowColor: 'transparent',
+        },
+        headerTintColor: '#EBF6FE',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerTitleAlign: 'center',
+        headerShadowVisible:false,
+        tabBarStyle:{
+          backgroundColor:'#EBF5FF',
+        },
+        tabBarActiveTintColor:'#145A32',
+        tabBarInactiveTintColor:'#02223B',
+        tabBarShowLabel:false
+      }}
+      >
+        <Tabs.Screen name="Dashboard" options={
+          {
+            headerShown:true,
+            title: `Dashboard`,
+            tabBarIcon: ({ color,focused}) => (
+              <FontAwesome6 name="grip" size={ focused?25:18} color={color} />
+            ),
+            headerLeft: () => (
+              <TouchableOpacity
+              onPress={()=>router.push('/UserNotifications')}
+              style={{marginLeft:20}}
+              >
+              <View>
+              <FontAwesome6 name="bell" size={20} color="#EBF6FE" />
+              {hasUnreadNotifications && (
+              <View
+              style={{
+              position: 'absolute',
+              top: -2,
+              right: -2,
+              width: 8,
+              height: 8,
+              borderRadius: 4,
+              backgroundColor: 'red',
+            }}
+            />
+          )}
+          </View>
+          </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity
+              style={{marginRight:20}}
+              onPress={()=>router.push("/Profile")}
+              >
+                <FontAwesome6 
+                  name="user-circle" 
+                  size={20} 
+                  color="#EBF6FE" 
+                />
+              </TouchableOpacity>
+            )
+          }
+        }/>
+        <Tabs.Screen name="Orders" options={
+          {
+            headerShown:true,
+            title: 'Orders',
+            tabBarIcon: ({ color, size=10,focused }) => (
+              <FontAwesome6 
+              name="cart-shopping" 
+              size={focused ? 25:18} 
+              color={color} />
+            ),
+          }
+        }/>
+        <Tabs.Screen name="Analytics" options={
+          {
+            headerShown:true,
+            title: 'Analytics',
+            tabBarIcon: ({ color, size=10,focused }) => (
+              <FontAwesome6 
+              name="chart-line" 
+              size={focused ? 25:18} 
+              color={color} />
+            ),
+          }
+        }/>
+        <Tabs.Screen name="Profile" options={
+          {
+            headerShown:true,
+            title: 'Profile',
+            tabBarIcon: ({ color, size=10,focused }) => (
+              <FontAwesome6 
+              name="user" 
+              size={focused ? 25:18} 
+              color={color} />
+            ),
+          }
+        }/>
+      </Tabs>
+      <Toast/>
+      </View>
+  );
+}
